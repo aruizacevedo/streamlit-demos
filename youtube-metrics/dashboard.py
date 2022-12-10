26c207631997c4b4c66b05b4ea34cb70c4c8d8dc
Andres Ruiz Acevedo <andresruizaceved@gmail.com>
2022-12-10 22:11:26 +1100

    Load CSV data

--- youtube-metrics/dashboard.py
+++ youtube-metrics/dashboard.py
@@ -9,8 +9,12 @@ from datetime import datetime
 # Define functions
 
 
-
 # Load data
+df_agg = pd.read_csv("youtube-metrics/data/Aggregated_Metrics_By_Video.csv").iloc[1:,:]
+df_agg_sub = pd.read_csv("youtube-metrics/data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv")
+df_comments = pd.read_csv("youtube-metrics/data/All_Comments_Final.csv")
+df_time = pd.read_csv("youtube-metrics/data/Video_Performance_Over_Time.csv")
+
 
 
 # Engineer data
