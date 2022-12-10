2b90d051cf0db4a1ea1d6594ad9bdb9837e5460e
Andres Ruiz Acevedo <andresruizaceved@gmail.com>
2022-12-10 21:54:33 +1100

    Init dashboard.py

---
+++ youtube-metrics/dashboard.py
@@ -0,0 +1,23 @@
+# Import libs
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+from datetime import datetime
+
+# Define functions
+
+
+
+# Load data
+
+
+# Engineer data
+# - What metrics will be relevant?
+# - Difference from baseline
+# - Percent change by video
+
+# Build dashboard
+# - Total picture
+# - Individual video
